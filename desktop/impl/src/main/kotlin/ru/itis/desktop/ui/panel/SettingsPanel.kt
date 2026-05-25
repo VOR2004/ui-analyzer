b778c4eb283eb3e80f6f3b5363d677caddc660e2
VOR2004 <mr.vor2006@mail.ru>
2026-05-25 23:43:32 +0300

    Вынесение строковых констант. Переименован модуль: android -> android-runtime

--- desktop/impl/src/main/kotlin/ru/itis/desktop/ui/panel/SettingsPanel.kt
+++ desktop/impl/src/main/kotlin/ru/itis/desktop/ui/panel/SettingsPanel.kt
@@ -8,12 +8,10 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
-import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextOverflow
